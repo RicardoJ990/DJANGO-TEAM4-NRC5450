3009f0e9680a96e38fda31bd0ab0611201e47b83
Dylan Ismael Jiménez Quingalombo <85418037+zdyLaNz@users.noreply.github.com>
2022-12-18 20:29:36 -0500

    menu
    menu

---
+++ Web_MisDelicias/menu.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menú</title>
+    <!--<link rel="shortcut icon" href="favicon.png">-->
+    <link rel="stylesheet" href="assets/css/estilos_menu.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+</head>
+
+<body class="hidden">
+    <div class="centrado" id="onload">
+        <div class="lds-facebook">
+            
+        </div>
+    </div>
+    <header>
+        <nav id="nav" class="nav1">
+            <div class="contenedor-nav">
+                <div class="logo">
+                <img src="assets/images/logosmall.png" alt="">
+                </div>
+                <div class="enlaces" id="enlaces">
+                    <a href="#" id="enlace-inicio" class="btn-header">Inicio</a>
+                    <a href="#" id="enlace-servicio" class="btn-header">Menú</a>
+                    <a href="#" id="enlace-equipo" class="btn-header">Platos fuertes</a>
+                    <!--<a href="#" id="enlace-trabajo" class="btn-header">Postres y bebidas</a>-->
+                    <a href="#" id="enlace-contacto" class="btn-header">Contáctanos</a>
+                    <a href="index.php" class="btn-header">Salir</a>
+                </div>
+                <div class="icono" id="open">
+                    <span>&#9776;</span>
+                </div>
+            </div>
+        </nav>
+        <div class="textos">
+           <h1></h1>
+            <h2></h2>
+            
+        </div>
+    </header>
+    <main>
+        
+        <section class="about" id="servicio">
+            <div class="contenedor">
+                <h3>Menús Del Día</h3>
+                <p class="after">Disfrútalos en familia</p>
+                <div class="servicios">
+                    <div class="caja-servicios">
+                        <img src="assets/images/menu1.jpeg" alt="">
+                        <h4>Menú #1</h4>
+                        <p></p>
+                    </div>
+                    <div class="caja-servicios">
+                        <img src="assets/images/menu 2.jpeg" alt="">
+                        <h4>Menú #2</h4>
+                        <p></p>
+                    </div>
+                    <div class="caja-servicios">
+                        <img src="assets/images/menu 3.jpeg" alt="">
+                        <h4>Menú #3</h4>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="team contenedor" id="equipo">
+            <h3>Platos Fuertes</h3>
+            <p class="after">Elige el de tu preferencia</p>
+            <div class="card">
+                <div class="content-card">
+                    <div class="people">
+                        <img src="assets/images/platillo2.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Lomo</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo3.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Milanesa</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo4.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Carne Salteada</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo5.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Estofado de Carne</h4>
+                        <p>Acompañado con arroz y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo6.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Costillas</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo7.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Spaguetti</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo8.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Apanado</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo9.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Brochetas</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo10.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Menestras</h4>
+                        <p>Acompañado con arroz y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo11.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Alitas</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo12.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Pollo frito</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+                <div class="content-card">
+                    <div class="people">
+                    <img src="assets/images/platillo1.jpeg" alt="">
+                    </div>
+                    <div class="texto-team">
+                        <h4>Salmón</h4>
+                        <p>Acompañado de Papas fritas y ensalada.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer id="contacto">
+        <div class="footer contenedor">
+            <div class="marca-logo">
+            <img src="assets/images/logosmall.png" alt="">
+            </div>
+            <div class="iconos">
+                
+                <a href="https://youtube.com/shorts/G0q21WQO2AY?feature=share" target="_blank"><i class="fab fa-youtube"></i></a>
+                <a href="https://www.facebook.com/MisDelicias-Restaurant-104867142321667" target="_blank"><i class="fab fa-facebook-square"></i>
+                <a href="https://github.com/ViankaH/5437_G4INGSWI/graphs/contributors" target="_blank"><i class="fab fa-github"></i></a>
+            </div>
+            <p>En este perfil amamos la comida de Mis Delicias.</p>
+        </div>
+
+    </footer>
+    </main>
+    
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/filtro.js"></script>
+</body>
+</html>
